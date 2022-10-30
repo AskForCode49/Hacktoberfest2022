30746e23f82f3176522cf90a5ecfa6decdf27652
AskForCode49 <116974120+AskForCode49@users.noreply.github.com>
2022-10-30 10:53:49 +0530

    Update puzzle.jsx

--- src/15puzzleComponents/puzzle.jsx
+++ src/15puzzleComponents/puzzle.jsx
@@ -31,54 +31,8 @@ class Puzzle extends Component {
     render() {
         let classNames;
         return (
+<div>
 
-            <div style={{backgroundColor: "#57407c"}}
-                 className={'full-height'}
-            >
-                <Navbar/>
-                <div className={'justify-content-around '}
-                     style={{textAlign: "Center"}}>
-                    <div style={{textAlign: "center", height: "440px", width: "440px", margin: 'auto'}}
-                         className={"m-5"}>
-                        <FlipMove
-                            duration={FLIP_DURATION}
-                            easing="cubic-bezier(.12,.36,.14,1.2)"
-                        >
-                            {this.state.squares.map((stt) =>
-                                <div key={stt.value}
-                                     className={stt.value === 0 ? "square " : stt.value % 2 === 0 ? 'square shadow correct pt-1' : 'square shadow painted pt-1'}
-                                >
-                                    {stt.value === 0 ? "" : stt.value}
-                                </div>
-                            )}
-                            <br/>
-                        </FlipMove>
-                        <button className={"btn btn-secondary"} onClick={this.balsal}>Animate</button>
-                    </div>
-
-                    {/*<FlipMove*/}
-                    {/*    duration={FLIP_DURATION}*/}
-                    {/*          easing="cubic-bezier(.12,.36,.14,1.2)" >*/}
-                    {/*    <line key={5464} style={{strokeWidth: "3px", stroke: '#999'}} x2={299} y2={186}*/}
-                    {/*          x1={(this.state.squares[0].value + 1) * 50}*/}
-                    {/*          y1={(this.state.squares[0].value + 1) * 50}></line>*/}
-                    {/*    <div key={this.state.squares[0].value}*/}
-                    {/*         className={'square shadow correct pt-1'}>*/}
-                    {/*        {this.state.squares[0].value}*/}
-                    {/*    </div>*/}
-                    {/*    <div key={this.state.squares[1].value}*/}
-                    {/*         className={'square shadow correct pt-1'}>*/}
-                    {/*        {this.state.squares[1].value}*/}
-                    {/*    </div>*/}
-                    {/*</FlipMove>*/}
-                    {/*/!*<SvgLines animate={true} duration={1000}>*!/*/}
-                    {/*/!*    <svg viewBox="0 0 960 500">*!/*/}
-                    {/*/!*        <line key={5464} style={{strokeWidth: "3px", stroke: '#999'}} x2={299} y2={186}*!/*/}
-                    {/*/!*              x1={(this.state.squares[0].value + 1) * 50}*!/*/}
-                    {/*/!*              y1={(this.state.squares[0].value + 1) * 50}></line>*!/*/}
-                    {/*/!*    </svg>*!/*/}
-                    {/*/!*</SvgLines>*!/*/}
-                </div>
 
             </div>
 
